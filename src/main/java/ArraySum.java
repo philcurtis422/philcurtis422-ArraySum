7dbee66389cb9b68d4fad384091bc8494a1d6411
philcurtis422 <philcurtis1010@gmail.com>
2023-03-08 22:47:31 +0000

    Sum Array Lab

--- src/main/java/ArraySum.java
+++ src/main/java/ArraySum.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class ArraySum {
     /**
      * This method should return the sum of all ints in an array - this is a very useful trick to know!
@@ -7,7 +9,15 @@ public class ArraySum {
      * @return the sum of all values in arr.
      */
     public int sum(int[] arr){
+        int sum = 0;
 
-        return 0;
+        for (int i = 0; i < arr.length; i++){
+            sum = sum + arr[i];
+        
+            
+        }
+            
+        return sum;
+        
     }
 }
